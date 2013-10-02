cfe8d3dbeeb69699e8f57c0765c52852e9995f9b
Andrey Kashin <iiotep9huy@gmail.com>
2013-10-02 16:07:10 +0400

    hello file updated

--- hello.py
+++ hello.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
 
-print("Hello, world!")
+
+import msgpack
+from cocaine.decorators import http
+
+from cocaine.worker import Worker
+from cocaine.logging import Logger
+
+@http
+def main(request, response):
+    response.write("Hello, world!")
+    response.close()
+
+
+W = Worker()
+W.run({
+    'doIt': main,
+})
